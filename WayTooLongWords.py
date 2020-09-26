a3b90fc866429b2cf5d83a99f472359683573a3c
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-09-26 16:55:59 +0600

    Update WayTooLongWords.py

--- WayTooLongWords.py
+++ WayTooLongWords.py
@@ -1,1 +1,18 @@
-#https://codeforces.com/problemset/problem/71/A
+# https://codeforces.com/problemset/problem/71/A
+
+n = int(input())
+my_list = []
+my_list1 = []
+for i in range(n):
+    word = input()
+    my_list.append(word)
+
+for i in my_list:
+    if len(i) < 11:
+        my_list1.append(i)
+    elif len(i) > 10:
+        l = str(len(i) - 2)
+        my_list1.append(i[0] + l + i[-1])
+
+for i in my_list1:
+    print(i)
