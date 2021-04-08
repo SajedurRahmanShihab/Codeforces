8fb8ed63bcc613d79e52e096671775cb1348fa58
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2021-04-08 18:41:40 +0600

    accepted

---
+++ DominoPiling.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/50/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int result = (m*n)/2;
+    cout<<result;
+}
